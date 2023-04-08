fa2500a348a01d9baf48cdb37a276b8c65bf2929
mgo <maxime.goossens@gmail.com>
2023-04-08 19:32:51 +0100

    Fix styling of layer tabs

--- src/components/HotspotsMap/LayerTabs.tsx
+++ src/components/HotspotsMap/LayerTabs.tsx
@@ -89,8 +89,7 @@ export default function LayerTabs() {
               className={clsx(
                 "font-semi-bold flex w-full cursor-pointer items-center gap-2 rounded-xl px-3 py-1.5 text-sm leading-5 ring-opacity-60 ring-offset-1 transition focus:outline-none focus:ring-2 ui-selected:shadow",
                 "ring-zinc-700 ring-offset-zinc-400/30 ui-selected:bg-zinc-400/30 ui-selected:text-gray-700 ui-not-selected:text-zinc-500 ui-not-selected:hover:bg-zinc-300 ui-not-selected:hover:text-zinc-700",
-                "dark:ring-white dark:ring-offset-gray-400 ui-selected:dark:bg-zinc-500/30 ui-selected:dark:text-white ui-not-selected:dark:hover:bg-zinc-700",
-                ""
+                "dark:ring-white dark:ring-offset-gray-400 ui-selected:dark:bg-zinc-500/30 ui-selected:dark:text-white ui-not-selected:dark:text-zinc-300 ui-not-selected:dark:hover:bg-zinc-700"
               )}
             >
               <Icon className="h-7 w-7" />
