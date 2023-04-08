83e378bfb99e662eb1c44848a80ef1917c4907bb
mgo <maxime.goossens@gmail.com>
2023-04-08 13:49:52 +0100

    Add helium mobile hotspots layer

--- src/components/HotspotsMap/MobileHotspotLayers.tsx
+++ src/components/HotspotsMap/MobileHotspotLayers.tsx
@@ -1,6 +1,5 @@
 import { Layer, Source } from "react-map-gl"
 import {
-  IOT_HOTSPOT_HEXES_LAYER,
   MIN_HEXES_ZOOM,
   MIN_MAP_ZOOM,
   POINTS_AND_HEXES_OVERLAP,
@@ -8,29 +7,37 @@ import {
   getHexFillStyle,
 } from "./utils"
 
-const LAYER_COLOR = "#27ac92"
+const LAYER_COLOR = "#D23E72"
 
-export default function IotHotspotLayers() {
+export default function MobileHotspotLayer() {
   return (
     <>
       <Source
+        id="cell_points_source"
         type="vector"
-        url="https://mt.hotspotty.org/helium_iot_points"
+        url="https://hotspot-tileserver.helium.wtf/public.cell_points.json"
         minzoom={MIN_MAP_ZOOM}
         maxzoom={MIN_HEXES_ZOOM + POINTS_AND_HEXES_OVERLAP}
       >
         <Layer
+          source="cell_points_source"
+          source-layer="public.cell_points"
+          id="cell_points"
           type="circle"
-          source-layer="helium_iot"
           maxzoom={MIN_HEXES_ZOOM + POINTS_AND_HEXES_OVERLAP}
           paint={getBlurredPointStyle(LAYER_COLOR)}
         />
       </Source>
-      <Source type="vector" url="https://mt.hotspotty.org/helium_iot_hexes">
+      <Source
+        id="cell_hexes_source"
+        type="vector"
+        url="https://hotspot-tileserver.helium.wtf/public.cell_h3_res8.json"
+      >
         <Layer
-          id={IOT_HOTSPOT_HEXES_LAYER}
+          id="cell_hexes"
           type="fill"
-          source-layer="helium_iot"
+          source="cell_hexes_source"
+          source-layer="public.cell_h3_res8"
           paint={getHexFillStyle(LAYER_COLOR)}
         />
       </Source>
