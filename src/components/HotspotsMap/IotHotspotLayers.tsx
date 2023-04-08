00d433bbad346a5bd8b89965bd88ff33b8191fbc
mgo <maxime.goossens@gmail.com>
2023-04-08 12:53:23 +0100

    Remove unneeded constants and attributes in the iot layers.

--- src/components/HotspotsMap/IotHotspotLayers.tsx
+++ src/components/HotspotsMap/IotHotspotLayers.tsx
@@ -1,38 +1,25 @@
 import { Layer, Source } from "react-map-gl"
 import { MIN_MAP_ZOOM } from "./constants"
 
-export default function HotspotsLayer() {
+export default function IotHotspotLayers() {
   return (
     <>
       <Source
-        id={HELIUM_IOT_POINTS_SOURCE}
         type="vector"
         url="https://mt.hotspotty.org/helium_iot_points"
         minzoom={MIN_MAP_ZOOM}
         maxzoom={MIN_HEXES_ZOOM + POINTS_AND_HEXES_OVERLAP}
       >
         <Layer
-          id="helium_iot_hotspots_circle"
           type="circle"
-          source={HELIUM_IOT_POINTS_SOURCE}
           source-layer="helium_iot"
           maxzoom={MIN_HEXES_ZOOM + POINTS_AND_HEXES_OVERLAP}
           paint={blurredOutlineStyle}
         />
       </Source>
 
-      <Source
-        id={HELIUM_IOT_HEXES_SOURCE}
-        type="vector"
-        url="https://mt.hotspotty.org/helium_iot_hexes"
-      >
-        <Layer
-          id="helium_iot_hotspots_fill"
-          type="fill"
-          source={HELIUM_IOT_HEXES_SOURCE}
-          source-layer="helium_iot"
-          paint={hexFillStyle}
-        />
+      <Source type="vector" url="https://mt.hotspotty.org/helium_iot_hexes">
+        <Layer type="fill" source-layer="helium_iot" paint={hexFillStyle} />
       </Source>
     </>
   )
@@ -41,9 +28,6 @@ export default function HotspotsLayer() {
 //
 // Utils
 //
-
-const HELIUM_IOT_POINTS_SOURCE = "helium_iot_points"
-const HELIUM_IOT_HEXES_SOURCE = "helium_iot_hexes"
 
 const CELL_COLOR = "#27ac92"
 
