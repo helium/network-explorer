2e0df21dd36943e1dd01d37683fecb2a4debf405
mgo <maxime.goossens@gmail.com>
2023-04-09 21:05:32 +0100

    Use nested route and SSR for hex details page. Fly to hex under certain circumstances.

---
+++ src/app/(hotspots-map)/layout.tsx
@@ -0,0 +1,6 @@
+import HotspotsMap from "@/components/HotspotsMap"
+import React from "react"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <HotspotsMap>{children}</HotspotsMap>
+}
--- src/components/HotspotsMap/index.tsx
+++ src/components/HotspotsMap/index.tsx
@@ -1,10 +1,15 @@
 "use client"
 
+import { cellToLatLng, cellsToMultiPolygon, getResolution } from "h3-js"
 import "mapbox-gl/dist/mapbox-gl.css"
 import { useTheme } from "next-themes"
-import { useCallback, useMemo, useRef, useState } from "react"
-import Map, { MapLayerMouseEvent, MapRef } from "react-map-gl"
-import HexDetails from "./HexDetails"
+import {
+  usePathname,
+  useRouter,
+  useSelectedLayoutSegments,
+} from "next/navigation"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import Map, { Layer, MapLayerMouseEvent, MapRef, Source } from "react-map-gl"
 import LayerTabs from "./LayerTabs"
 import {
   HexFeatureDetails,
@@ -12,13 +17,22 @@ import {
   MAP_CONTAINER_STYLE,
   MAX_MAP_ZOOM,
   MIN_MAP_ZOOM,
+  ZOOM_BY_HEX_RESOLUTION,
+  hexOutlineStyle,
 } from "./utils"
 
-export default function HotspotsMap() {
-  const mapRef = useRef<MapRef>(null)
-  const [cursor, setCursor] = useState("")
-  const [selectedHex, setSelectedHex] = useState<HexFeatureDetails | null>(null)
+export default function HotspotsMap({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   const { resolvedTheme } = useTheme()
+  const router = useRouter()
+  const pathname = usePathname()
+  const segments = useSelectedLayoutSegments()
+  const mapRef = useRef<MapRef>(null)
+  const [selectedHex, setSelectedHex] = useState<HexFeatureDetails | null>(null)
+  const [cursor, setCursor] = useState("")
 
   const mapStyleUrl = useMemo(() => {
     let key =
@@ -29,54 +43,68 @@ export default function HotspotsMap() {
     return `mapbox://styles/hotspotty/${key}?optimize=true`
   }, [resolvedTheme])
 
-  const onClick = useCallback((event: MapLayerMouseEvent) => {
-    if (!mapRef.current || !event.features) return
+  const selectHex = useCallback((hexId: string | null) => {
+    if (!hexId) {
+      setSelectedHex(null)
+      return
+    }
 
+    const selectedHex = {
+      hexId,
+      geojson: {
+        type: "MultiPolygon",
+        coordinates: cellsToMultiPolygon([hexId], true),
+      } as GeoJSON.Geometry,
+    }
+
+    setSelectedHex(selectedHex)
+
+    if (!mapRef.current) return
     const map = mapRef.current.getMap()
-
-    event.features.forEach(({ id, layer, properties }) => {
-      if (!id || layer.id !== "hexes_layer" || !properties?.id) return
-      const selectedHex = {
-        hexFeatureId: id as number,
-        hexId: properties.id as string,
-        sourceId: layer.source as string,
-        sourceLayer: layer["source-layer"] as string,
-      }
-
-      setSelectedHex(selectedHex)
-
-      map.setFeatureState(
-        {
-          source: selectedHex.sourceId,
-          sourceLayer: selectedHex.sourceLayer,
-          id: selectedHex.hexFeatureId,
-        },
-        { selected: false }
-      )
-    })
+    const [lat, lng] = cellToLatLng(hexId)
+    const bounds = map.getBounds()
+    const zoom = map.getZoom()
+    const hexResolution = getResolution(hexId)
+    const newZoom = ZOOM_BY_HEX_RESOLUTION[hexResolution]
+    if (zoom < newZoom - 3 || !bounds.contains([lng, lat])) {
+      // Fly to the hex if it's not visible in the current viewport, or if it's not zoomed in enough
+      map.flyTo({ center: [lng, lat], zoom: newZoom })
+    }
   }, [])
+
+  const selectHexByPathname = useCallback(() => {
+    if (!mapRef.current) return
+
+    if (segments.length === 2 && segments[0] === "hex") {
+      const hexId = segments[1]
+      if (selectedHex?.hexId !== hexId) {
+        selectHex(hexId)
+      }
+    } else if (pathname === "/" && selectedHex?.hexId) {
+      selectHex(null)
+    }
+  }, [pathname, segments, selectHex, selectedHex?.hexId])
+
+  useEffect(() => {
+    selectHexByPathname()
+  }, [selectHexByPathname])
+
+  const onClick = useCallback(
+    (event: MapLayerMouseEvent) => {
+      event.features?.forEach(({ layer, properties }) => {
+        if (layer.id !== "hexes_layer" || !properties?.id) return
+        if (selectedHex?.hexId === properties.id) {
+          router.push("/")
+        } else {
+          router.push(`/hex/${properties.id}`)
+        }
+      })
+    },
+    [router, selectedHex?.hexId]
+  )
 
   const onMouseEnter = useCallback(() => setCursor("pointer"), [])
   const onMouseLeave = useCallback(() => setCursor(""), [])
-
-  const closeHexDetails = useCallback(() => {
-    if (!mapRef.current || !selectedHex) return
-
-    const map = mapRef.current.getMap()
-
-    map.setFeatureState(
-      {
-        source: selectedHex.sourceId,
-        sourceLayer: selectedHex.sourceLayer,
-        id: selectedHex.hexFeatureId,
-      },
-      { selected: true }
-    )
-
-    setSelectedHex(null)
-  }, [selectedHex])
-
-  if (!process.env.NEXT_PUBLIC_MAPBOX_KEY) return null
 
   return (
     <Map
@@ -87,17 +115,24 @@ export default function HotspotsMap() {
       mapStyle={mapStyleUrl}
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
       interactiveLayerIds={["hexes_layer"]}
+      onLoad={selectHexByPathname}
       onClick={onClick}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
       cursor={cursor}
       ref={mapRef}
+      attributionControl={false}
+      logoPosition="bottom-right"
     >
+      {children}
       <div className="fixed bottom-6 z-10 flex w-full justify-center">
         <LayerTabs />
       </div>
-
-      <HexDetails selectedHex={selectedHex} onClose={closeHexDetails} />
+      {selectedHex && (
+        <Source type="geojson" data={selectedHex.geojson}>
+          <Layer type="line" paint={hexOutlineStyle} />
+        </Source>
+      )}
     </Map>
   )
 }
