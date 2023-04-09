8e96a4644d479c4db87ccab734c0c3a51838a04f
mgo <maxime.goossens@gmail.com>
2023-04-09 22:47:43 +0100

    Remove nova maptiles

--- src/components/HotspotsMap/LayerTabs.tsx
+++ src/components/HotspotsMap/LayerTabs.tsx
@@ -20,59 +20,31 @@ export interface NetworkCoverageLayerOption {
 
 const layerOptions: NetworkCoverageLayerOption[] = [
   {
-    name: "IOT - Hotspotty",
+    name: "IOT",
     icon: HeliumIotIcon,
     color: "#27EE76",
     sourceDomain: process.env.NEXT_PUBLIC_TILESERVER_URL!,
     points: {
-      sourcePath: "helium_iot_points",
-      sourceLayer: "helium_iot",
+      sourcePath: "public.helium_iot_points.json",
+      sourceLayer: "public.helium_iot_points",
     },
     hexes: {
-      sourcePath: "helium_iot_hexes",
-      sourceLayer: "helium_iot",
+      sourcePath: "public.helium_iot_hexes.json",
+      sourceLayer: "public.helium_iot_hexes",
     },
   },
   {
-    name: "IOT - Nova Labs",
-    icon: HeliumIotIcon,
-    color: "#27EE76",
-    sourceDomain: "https://hotspot-tileserver.helium.wtf",
-    points: {
-      sourcePath: "public.points.json",
-      sourceLayer: "public.points",
-    },
-    hexes: {
-      sourcePath: "public.h3_res8.json",
-      sourceLayer: "public.h3_res8",
-    },
-  },
-  {
-    name: "MOBILE - Nova Labs",
-    icon: HeliumMobileIcon,
-    color: "#009FF9",
-    sourceDomain: "https://hotspot-tileserver.helium.wtf",
-    points: {
-      sourcePath: "public.cell_points.json",
-      sourceLayer: "public.cell_points",
-    },
-    hexes: {
-      sourcePath: "public.cell_h3_res8.json",
-      sourceLayer: "public.cell_h3_res8",
-    },
-  },
-  {
-    name: "MOBILE - Hotspotty",
+    name: "MOBILE",
     icon: HeliumMobileIcon,
     color: "#009FF9",
     sourceDomain: process.env.NEXT_PUBLIC_TILESERVER_URL!,
     points: {
-      sourcePath: "helium_mobile_points",
-      sourceLayer: "helium_mobile",
+      sourcePath: "public.helium_mobile_points.json",
+      sourceLayer: "public.helium_mobile_points",
     },
     hexes: {
-      sourcePath: "helium_mobile_hexes",
-      sourceLayer: "helium_mobile",
+      sourcePath: "public.helium_mobile_hexes.json",
+      sourceLayer: "public.helium_mobile_hexes",
     },
   },
 ]
@@ -87,9 +59,9 @@ export default function LayerTabs() {
             <Tab
               key={name}
               className={clsx(
-                "font-semi-bold flex w-full cursor-pointer items-center gap-2 rounded-xl px-3 py-1.5 text-sm leading-5 ring-opacity-60 ring-offset-1 transition focus:outline-none focus:ring-2 ui-selected:shadow",
+                "font-semi-bold flex w-full cursor-pointer items-center gap-2 rounded-xl px-3 py-2 text-sm leading-5 ring-opacity-60 ring-offset-1 transition focus:outline-none focus:ring-2 ui-selected:shadow",
                 "ring-zinc-700 ring-offset-zinc-400/30 ui-selected:bg-zinc-400/30 ui-selected:text-gray-700 ui-not-selected:text-zinc-500 ui-not-selected:hover:bg-zinc-300 ui-not-selected:hover:text-zinc-700",
-                "dark:ring-white dark:ring-offset-gray-400 ui-selected:dark:bg-zinc-500/30 ui-selected:dark:text-white ui-not-selected:dark:text-zinc-300 ui-not-selected:dark:hover:bg-zinc-700"
+                "dark:ring-white dark:ring-offset-gray-400 ui-selected:dark:bg-zinc-500/30 ui-selected:dark:text-white ui-not-selected:dark:text-zinc-300 ui-not-selected:dark:hover:bg-zinc-700/50"
               )}
             >
               <Icon className="h-7 w-7" />
