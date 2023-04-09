049389a1c456b0b42fb18b09578e9e8de93a0ed7
mgo <maxime.goossens@gmail.com>
2023-04-09 22:44:54 +0100

    Load hex hotspots.

--- src/app/(hotspots-map)/hex/[hexId]/page.tsx
+++ src/app/(hotspots-map)/hex/[hexId]/page.tsx
@@ -1,7 +1,9 @@
+import HexHotspots from "@/components/HexHotspots"
 import { XMarkIcon } from "@heroicons/react/24/outline"
 import { isValidCell } from "h3-js"
 import Link from "next/link"
 import { redirect } from "next/navigation"
+import { Suspense } from "react"
 
 export default async function Page({ params }: { params: { hexId: string } }) {
   if (!isValidCell(params.hexId)) redirect("/")
@@ -9,11 +11,15 @@ export default async function Page({ params }: { params: { hexId: string } }) {
   return (
     <div className="absolute inset-y-6 left-6 z-50 flex w-80 flex-col gap-4 rounded-xl bg-white/30 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/30 dark:text-zinc-200 dark:ring-white/10">
       <div className="flex w-full items-center p-2">
-        <div className="flex-1">{params.hexId}</div>
+        <div className="flex-1">Hex: {params.hexId}</div>
         <Link href="/">
           <XMarkIcon className="h-6 w-6 text-zinc-500 transition hover:text-zinc-700 dark:text-zinc-400 hover:dark:text-zinc-100" />
         </Link>
       </div>
+      <Suspense fallback={<p>Loading hotspots...</p>}>
+        {/* @ts-expect-error Async Server Component */}
+        <HexHotspots hexId={params.hexId} />
+      </Suspense>
     </div>
   )
 }
---
+++ src/components/HexHotspots.tsx
@@ -0,0 +1,99 @@
+import animalHash from "angry-purple-tiger"
+import clsx from "clsx"
+import Image from "next/image"
+
+interface SmallCell {
+  cell_id: string
+  "24h_rewards_mobile": number
+}
+
+interface Hotspot {
+  hotspot_id: string
+  "24h_rewards_iot": number
+  avatarUrl: string
+  cells: SmallCell[]
+}
+
+interface HexData {
+  hex: string
+  resolution: number
+  hotspots: Hotspot[]
+}
+
+export default async function HexHotspots({ hexId }: { hexId: string }) {
+  const hexData = (await fetch(
+    `${process.env.HOTSPOTTY_EXPLORER_API_URL}/hex/${hexId}`,
+    {
+      headers: {
+        Authorization: `bearer ${process.env.HOTSPOTTY_EXPLORER_API_TOKEN}`,
+      },
+    }
+  ).then((res) => res.json())) as HexData
+
+  const hotspots = hexData.hotspots
+
+  hotspots.sort(
+    (hotspot1, hotspot2) =>
+      hotspot2["24h_rewards_iot"] - hotspot1["24h_rewards_iot"]
+  )
+
+  return (
+    <ul
+      role="list"
+      className="flex-1 divide-y divide-gray-200 overflow-y-auto dark:divide-white/10"
+    >
+      {hotspots.map((hotspot) => {
+        const hotspotName = animalHash(hotspot.hotspot_id)
+        let subtitle = "IoT hotspot"
+        if (hotspot.cells.length === 1) subtitle = "One small cell attached"
+        if (hotspot.cells.length > 1) {
+          subtitle = `${hotspot.cells.length} small cells attached`
+        }
+        return (
+          <li key={hotspot.hotspot_id}>
+            <div className="group relative flex items-center px-5 py-6">
+              <a
+                href={`https://app.hotspotty.net/devices/${hotspot.hotspot_id}/status`}
+                target="_blank"
+                className="-m-1 block flex-1 p-1"
+              >
+                <div
+                  className="absolute inset-0 group-hover:bg-gray-50 dark:group-hover:bg-zinc-700/30"
+                  aria-hidden="true"
+                />
+                <div className="relative flex min-w-0 flex-1 items-center">
+                  <span className="relative inline-block flex-shrink-0">
+                    <Image
+                      className="h-10 w-10 rounded-full"
+                      width={40}
+                      height={40}
+                      src={hotspot.avatarUrl || "/logo192.png"}
+                      alt={hotspotName}
+                    />
+                    <span
+                      className={clsx(
+                        hotspot["24h_rewards_iot"] > 0
+                          ? "bg-green-400"
+                          : "bg-gray-300",
+                        "absolute right-0 top-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white"
+                      )}
+                      aria-hidden="true"
+                    />
+                  </span>
+                  <div className="ml-4 truncate">
+                    <p className="truncate text-sm font-medium text-gray-900 dark:text-zinc-200">
+                      {hotspotName}
+                    </p>
+                    <p className="truncate text-sm text-gray-500 dark:text-zinc-400">
+                      {subtitle}
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
