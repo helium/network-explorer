c3ba83add73346ee8a88c021d74a51c0cd45e5de
mgo <maxime.goossens@gmail.com>
2023-04-10 08:14:53 +0100

    Add labels to hex layers

--- src/components/HotspotsMap/NetworkCoverageLayer.tsx
+++ src/components/HotspotsMap/NetworkCoverageLayer.tsx
@@ -1,11 +1,15 @@
+import { useTheme } from "next-themes"
 import { Fragment } from "react"
 import { Layer, Source } from "react-map-gl"
 import { NetworkCoverageLayerOption } from "./LayerTabs"
 import {
   MIN_HEXES_ZOOM,
+  MIN_HEX_LABELS_ZOOM,
   POINTS_AND_HEXES_OVERLAP,
   getBlurredPointStyle,
   getHexFillStyle,
+  getHexLabelStyle,
+  hexLabelLayout,
 } from "./utils"
 
 export default function NetworkCoverageLayer({
@@ -14,13 +18,13 @@ export default function NetworkCoverageLayer({
 }: {
   layer: NetworkCoverageLayerOption
 }) {
+  const { resolvedTheme } = useTheme()
   return (
     <Fragment {...props}>
       <Source
         id="points_source"
         type="vector"
         url={`${sourceDomain}/${points.sourcePath}`}
-        maxzoom={MIN_HEXES_ZOOM + POINTS_AND_HEXES_OVERLAP}
       >
         <Layer
           id="points_layer"
@@ -29,12 +33,19 @@ export default function NetworkCoverageLayer({
           maxzoom={MIN_HEXES_ZOOM + POINTS_AND_HEXES_OVERLAP}
           paint={getBlurredPointStyle(color)}
         />
+        <Layer
+          id="hotspot_count_labels"
+          type="symbol"
+          source-layer={points.sourceLayer}
+          minzoom={MIN_HEX_LABELS_ZOOM}
+          layout={hexLabelLayout}
+          paint={getHexLabelStyle(resolvedTheme)}
+        />
       </Source>
       <Source
         id="hexes_source"
         type="vector"
         url={`${sourceDomain}/${hexes.sourcePath}`}
-        minzoom={MIN_HEXES_ZOOM - POINTS_AND_HEXES_OVERLAP}
       >
         <Layer
           id="hexes_layer"
