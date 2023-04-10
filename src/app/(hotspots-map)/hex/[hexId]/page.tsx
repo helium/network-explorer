ef9e7f3fc48d905961c984e8e23501771fcac2f5
mgo <maxime.goossens@gmail.com>
2023-04-10 07:36:02 +0100

    Add loading skeleton for hex hotspots

--- src/app/(hotspots-map)/hex/[hexId]/page.tsx
+++ src/app/(hotspots-map)/hex/[hexId]/page.tsx
@@ -1,4 +1,5 @@
-import HexHotspots from "@/components/HexHotspots"
+import HexHotspots from "@/components/HotspotsMap/HexHotspots"
+import LoadingHexHotspots from "@/components/HotspotsMap/LoadingHexHotspots"
 import { XMarkIcon } from "@heroicons/react/24/outline"
 import { isValidCell } from "h3-js"
 import Link from "next/link"
@@ -9,14 +10,14 @@ export default async function Page({ params }: { params: { hexId: string } }) {
   if (!isValidCell(params.hexId)) redirect("/")
 
   return (
-    <div className="absolute bottom-6 left-4 right-4 top-24 z-50 flex w-auto flex-col gap-4 rounded-xl bg-white/30 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/30 dark:text-zinc-200 dark:ring-white/10 sm:inset-y-6 sm:left-6 sm:right-auto sm:w-80">
+    <div className="absolute bottom-6 left-4 right-4 top-24 z-50 flex w-auto flex-col gap-4 rounded-xl bg-white/30 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/30 dark:text-zinc-200 dark:ring-white/10 sm:bottom-auto sm:left-6 sm:right-auto sm:top-6 sm:max-h-[calc(100vh-3rem)] sm:w-80">
       <div className="flex w-full items-center p-2">
         <div className="flex-1">Hex: {params.hexId}</div>
         <Link href="/">
           <XMarkIcon className="h-6 w-6 text-zinc-500 transition hover:text-zinc-700 dark:text-zinc-400 hover:dark:text-zinc-100" />
         </Link>
       </div>
-      <Suspense fallback={<p>Loading hotspots...</p>}>
+      <Suspense fallback={<LoadingHexHotspots count={3} />}>
         {/* @ts-expect-error Async Server Component */}
         <HexHotspots hexId={params.hexId} />
       </Suspense>
--- src/components/HotspotsMap/HexHotspots.tsx
+++ src/components/HotspotsMap/HexHotspots.tsx
@@ -61,7 +61,7 @@ export default async function HexHotspots({ hexId }: { hexId: string }) {
                   className="absolute inset-0 group-hover:bg-gray-50 dark:group-hover:bg-zinc-700/30"
                   aria-hidden="true"
                 />
-                <div className="relative flex min-w-0 flex-1 items-center">
+                <div className="relative flex min-w-0 flex-1 items-center gap-4">
                   <span className="relative inline-block flex-shrink-0">
                     <Image
                       className="h-10 w-10 rounded-full"
@@ -74,13 +74,13 @@ export default async function HexHotspots({ hexId }: { hexId: string }) {
                       className={clsx(
                         hotspot["24h_rewards_iot"] > 0
                           ? "bg-green-400"
-                          : "bg-gray-300",
+                          : "bg-red-300",
                         "absolute right-0 top-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white"
                       )}
                       aria-hidden="true"
                     />
                   </span>
-                  <div className="ml-4 truncate">
+                  <div className="truncate">
                     <p className="truncate text-sm font-medium text-gray-900 dark:text-zinc-200">
                       {hotspotName}
                     </p>
