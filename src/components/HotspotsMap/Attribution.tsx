71a7d71cdf526bd8081fb86b125d9223d77c0ab0
mgo <maxime.goossens@gmail.com>
2023-04-11 19:47:38 +0100

    Remove the network selection tab. Set IOT coverage as default but still make MOBILE coverage available on /mobile route. Rework attribution component.

--- src/components/HotspotsMap/Attribution.tsx
+++ src/components/HotspotsMap/Attribution.tsx
@@ -1,20 +1,26 @@
+import clsx from "clsx"
 import Link from "next/link"
+import { HotspottyIcon } from "../icons/HotspottyIcon"
 
-export function Attribution() {
+export function Attribution({ className }: { className?: string }) {
   return (
-    <div className="absolute bottom-1 right-2.5 z-10">
-      <Link
-        href="https://hotspotty.net"
-        target="_blank"
-        aria-label="Powered by Hotspotty"
-      >
-        <span className="text-xs text-zinc-800 dark:text-zinc-200">
-          Powered by{" "}
-        </span>
-        <span className="text-base font-semibold text-zinc-800 dark:text-white">
-          Hotspotty
-        </span>
-      </Link>
-    </div>
+    <Link
+      className={clsx("group", className)}
+      href="https://hotspotty.net"
+      target="_blank"
+      aria-label="Powered by Hotspotty"
+    >
+      <div className="flex gap-3 rounded-xl bg-white/30 p-4 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/30 dark:ring-white/10">
+        <HotspottyIcon className="h-10 w-10" />
+        <div className="flex-1">
+          <div className="text-xs font-light text-zinc-800 dark:text-zinc-200">
+            Powered by{" "}
+          </div>
+          <div className="text-lg font-semibold text-zinc-800 group-hover:underline dark:text-white">
+            Hotspotty
+          </div>
+        </div>
+      </div>
+    </Link>
   )
 }
