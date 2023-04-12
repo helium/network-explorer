407fd1d6bd6d0f04662d4c1109a10e3aa4a5dfd9
mgo <maxime.goossens@gmail.com>
2023-04-12 11:06:55 +0100

    Rework style attribute into a tailwind class syntax.

--- src/components/HotspotsMap/Attribution.tsx
+++ src/components/HotspotsMap/Attribution.tsx
@@ -11,7 +11,7 @@ export function Attribution({ className }: { className?: string }) {
       aria-label="Powered by Hotspotty"
     >
       <div className="flex flex-row items-center gap-2 rounded-xl bg-white/30 px-4 py-3 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/30 dark:ring-white/10">
-        <HotspottyIcon className="h-6 w-6" style={{ color: "#9546ea" }} />
+        <HotspottyIcon className="h-6 w-6 text-[#9546ea]" />
         <span className="whitespace-nowrap font-medium text-zinc-800 group-hover:underline dark:text-zinc-200">
           Powered by Hotspotty
         </span>
