f47a5fabb82465fea8ec28b8a9ae66e7f6ff91cf
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2023-04-11 21:45:23 -0700

    Simplified svg & attribution
    slightly smaller attribution so its not the biggest thing on the page. :smile:

--- src/components/HotspotsMap/Attribution.tsx
+++ src/components/HotspotsMap/Attribution.tsx
@@ -10,16 +10,11 @@ export function Attribution({ className }: { className?: string }) {
       target="_blank"
       aria-label="Powered by Hotspotty"
     >
-      <div className="flex gap-3 rounded-xl bg-white/30 p-4 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/30 dark:ring-white/10">
-        <HotspottyIcon className="h-10 w-10" />
-        <div className="flex-1">
-          <div className="text-xs font-light text-zinc-800 dark:text-zinc-200">
-            Powered by{" "}
-          </div>
-          <div className="text-lg font-semibold text-zinc-800 group-hover:underline dark:text-white">
-            Hotspotty
-          </div>
-        </div>
+      <div className="flex flex-row items-center gap-2 rounded-xl bg-white/30 px-4 py-3 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/30 dark:ring-white/10">
+        <HotspottyIcon className="h-6 w-6" style={{ color: "#9546ea" }} />
+        <span className="whitespace-nowrap font-medium text-zinc-800 group-hover:underline dark:text-zinc-200">
+          Powered by Hotspotty
+        </span>
       </div>
     </Link>
   )
