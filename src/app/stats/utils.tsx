eb96df4dc0fb3939b81cc3993b8b6721e12d9e9d
mbthiery <mbthiery@gmail.com>
2023-04-28 09:43:16 -0400

    Revert "Add veHNT delegation graph"

--- src/app/stats/utils.tsx
+++ src/app/stats/utils.tsx
@@ -4,19 +4,12 @@ export const fetcher = async (url: string) => {
   return fetch(url).then((response) => response.json())
 }
 
-export const veHNTWoDecimal = (numberStr: string) => {
-  return parseInt(
-    numberStr
-      .split("")
-      .slice(0, numberStr.length - 8)
-      .join(""),
-    0
-  )
-}
-
 export const humanReadableVeHNT = (numberStr: string) => {
-  const numberWODecimal = veHNTWoDecimal(numberStr)
-  return numberWithCommas(numberWODecimal, 0)
+  const numberWODecimal = numberStr
+    .split("")
+    .slice(0, numberStr.length - 8)
+    .join("")
+  return numberWithCommas(parseInt(numberWODecimal, 0), 0)
 }
 
 export const ONE_DAY_UNIX = 60 * 60 * 24
