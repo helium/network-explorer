edc6a66f35c94341fe708043925a53d16f3e316a
mbthiery <mbthiery@gmail.com>
2023-05-07 11:16:12 -0400

    Revalidate stats every 5 mins

--- src/app/stats2/page.tsx
+++ src/app/stats2/page.tsx
@@ -1,6 +1,8 @@
 import { MapCover } from "@/components/MapCover"
 import { Stats } from "./components"
 
+export const revalidate = 60 * 5 // revalidate 5 minutes
+
 export default async function Page() {
   return (
     <MapCover title="Network Stats">
