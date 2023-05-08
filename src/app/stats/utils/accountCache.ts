2c7cc5c54842416c49fe334bbbd0ce2c61e10dcb
mbthiery <mbthiery@gmail.com>
2023-05-08 11:50:31 -0400

    Remove old rpc env + update rpc to mainnet

--- src/app/stats/utils/accountCache.ts
+++ src/app/stats/utils/accountCache.ts
@@ -2,9 +2,7 @@ import { AccountFetchCache } from "@helium/spl-utils"
 import { Connection } from "@solana/web3.js"
 
 const createAccountCache = () => {
-  const connection = new Connection(
-    process.env.SOLANA_RPC! || process.env.NEXT_PUBLIC_SOLANA_RPC!
-  )
+  const connection = new Connection(process.env.SOLANA_RPC!)
 
   return new AccountFetchCache({
     connection,
