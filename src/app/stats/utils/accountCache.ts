5fae5a21fd2b2b6486b3064a97fb5b0395eaf4fc
mbthiery <mbthiery@gmail.com>
2023-05-08 10:16:52 -0400

    Add comment for staging test

--- src/app/stats/utils/accountCache.ts
+++ src/app/stats/utils/accountCache.ts
@@ -2,6 +2,7 @@ import { AccountFetchCache } from "@helium/spl-utils"
 import { Connection } from "@solana/web3.js"
 
 const createAccountCache = () => {
+  // env var only available server side
   const connection = new Connection(process.env.SOLANA_RPC!)
 
   return new AccountFetchCache({
