9e82cad1b4a729a6e71f80b1772e439ef6a17449
mbthiery <mbthiery@gmail.com>
2023-05-16 11:18:04 -0400

    Add supply staked to stats

--- src/app/stats/components/HntInfo.tsx
+++ src/app/stats/components/HntInfo.tsx
@@ -3,10 +3,17 @@ import { StatsList } from "@/app/stats/components/StatsList"
 import { ONE_DAY_MS, fetcher } from "@/app/stats/utils"
 import { BN } from "@coral-xyz/anchor"
 import { currentEpoch } from "@helium/helium-sub-daos-sdk"
-import { MOBILE_MINT, amountAsNum } from "@helium/spl-utils"
+import {
+  HNT_MINT,
+  MOBILE_MINT,
+  amountAsNum,
+  humanReadable,
+} from "@helium/spl-utils"
 import { format } from "date-fns"
 import { fetchSubDaoEpochInfo } from "../../stats/utils/fetchSubDaoEpochInfo"
 import { fetchUnixTimestap } from "../../stats/utils/fetchUnixTimestamp"
+import { fetchGovernanceStats } from "../utils/fetchGovernanceMetrics"
+import { fetchMint } from "../utils/fetchMint"
 import { Countdown } from "./Countdown"
 
 const COINGECKO_HNT_URL =
@@ -14,10 +21,18 @@ const COINGECKO_HNT_URL =
 const NEXT_HALVENING = 1690848000 // unix time
 
 export const HntInfo = async () => {
-  const [unixTime, hntPrice] = await Promise.all([
+  const [unixTime, hntPrice, hntMint, governanceStats] = await Promise.all([
     fetchUnixTimestap(),
     fetcher(COINGECKO_HNT_URL),
+    fetchMint(HNT_MINT),
+    fetchGovernanceStats(),
   ])
+
+  const hntStakedTotal = governanceStats.network.total.hnt
+  const hntStakedPercent = hntStakedTotal
+    .mul(new BN(10000))
+    .div(new BN(hntMint.info?.info.supply))
+
   const epoch = currentEpoch(new BN(unixTime)).toNumber()
   const epochInfo = await fetchSubDaoEpochInfo(MOBILE_MINT)
   const lastEpochEnd = amountAsNum(epochInfo.info?.rewardsIssuedAt || 0, 0)
@@ -48,6 +63,14 @@ export const HntInfo = async () => {
       <StatItem
         label="Last Epoch Ended"
         value={format(new Date(lastEpochEnd * 1000), "Y/MM/dd HH:mm:ss")}
+      />
+      <StatItem
+        label="Supply Staked"
+        value={`${humanReadable(hntStakedPercent, 2)}%`}
+        tooltip={{
+          description:
+            "Percent of current HNT which is staked as veHNT on Realms",
+        }}
       />
     </StatsList>
   )
