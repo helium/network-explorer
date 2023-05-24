ce338776631f95f67f52cdbd9611e9f84cab0d03
mbthiery <mbthiery@gmail.com>
2023-05-24 16:04:30 -0400

    Render network usage

--- src/app/stats/components/Governance/index.tsx
+++ src/app/stats/components/Governance/index.tsx
@@ -1,28 +1,10 @@
-import clsx from "clsx"
-import { PropsWithChildren } from "react"
 import {
   fetchHntGovernanceStats,
   fetchSubDaoGovernanceStats,
 } from "../../utils/fetchGovernanceMetrics"
+import { GraphWrapper } from "../GraphWrapper"
 import { DelegationHistory } from "./DelegationHistory"
 import { MetricsRow } from "./MetricsRow"
-const GovernanceItem = ({
-  children,
-  label,
-}: PropsWithChildren<{ label: string }>) => {
-  return (
-    <div
-      className={clsx(
-        "flex-col gap-2 rounded-xl border p-4",
-        "border-zinc-900/5 bg-white text-zinc-800 shadow",
-        "dark:border-white/10 dark:bg-slate-900 dark:text-slate-100"
-      )}
-    >
-      <p className="pb-2 text-sm">{label}</p>
-      {children}
-    </div>
-  )
-}
 
 export const Governance = async () => {
   const [veHntStats, veIotMetrics, veMobileMetrics] = await Promise.all([
@@ -38,10 +20,10 @@ export const Governance = async () => {
         <h2 className="text-xl text-zinc-600 dark:text-zinc-100">
           HNT Governance
         </h2>
-        <GovernanceItem label="Delegation History (30 days)">
+        <GraphWrapper label="Delegation History (30 days)">
           {/* @ts-expect-error Async Server Component */}
           <DelegationHistory />
-        </GovernanceItem>
+        </GraphWrapper>
         <MetricsRow
           title="Network"
           icon="hnt"
