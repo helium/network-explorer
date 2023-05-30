a4c6c673955a30d405d82c590be3fc0b8377c846
mbthiery <mbthiery@gmail.com>
2023-05-30 16:31:41 -0400

    Fix delayed epoch info update

--- src/app/stats/utils/fetchSubDaoEpochInfo.ts
+++ src/app/stats/utils/fetchSubDaoEpochInfo.ts
@@ -7,15 +7,15 @@ import { IDL as subDaosIDL } from "@helium/idls/helium_sub_daos"
 import { cache } from "react"
 import { fetchIdlAccount } from "./fetchIdlAccount"
 
-const today = Math.floor(new Date().valueOf() / 1000)
-export const getSubDaoEpochInfo = (
+export const getSubDaoEpochInfo = async (
   subDaoMint: PublicKey,
   offset: number = 1
 ) => {
+  const now = Math.floor(new Date().valueOf() / 1000)
   const SUBDAO_KEY = subDaoKey(subDaoMint)[0]
   const sdeKey = subDaoEpochInfoKey(
     SUBDAO_KEY,
-    BigInt(today) - BigInt(ONE_DAY_UNIX * offset)
+    BigInt(now) - BigInt(ONE_DAY_UNIX * offset)
   )[0]
 
   return fetchIdlAccount<HeliumSubDaos>(
