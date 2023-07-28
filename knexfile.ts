47cbf6aaccc6c2704a47e0fbb89a8a678fc3f62b
mbthiery <mbthiery@gmail.com>
2023-07-28 15:24:55 -0400

    Add sqlite when no DB setup

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,22 @@
 import type { Knex } from "knex"
 require("dotenv").config()
 
-const config: Knex.Config = {
+// used for preview / if DATABASE_URL not set
+const SQLITE_CONFIG: Knex.Config = {
+  client: "better-sqlite3",
+  connection: {
+    filename: "./db.sqlite3",
+  },
+}
+
+const PG_CONFIG = {
   client: "postgresql",
   connection: process.env.DATABASE_URL,
   migrations: {
     extension: "ts",
   },
 }
+
+const config: Knex.Config = process.env.DATABASE_URL ? PG_CONFIG : SQLITE_CONFIG
 
 export default config
