90db06d42f2d5939803d36b56b7e2c5dedcdc8d5
mbthiery <mbthiery@gmail.com>
2023-08-21 15:53:31 -0400

    Minimize attribution size

--- src/components/HotspotsMap/Attribution.tsx
+++ src/components/HotspotsMap/Attribution.tsx
@@ -5,15 +5,15 @@ import { HotspottyIcon } from "../icons/HotspottyIcon"
 export function Attribution({ className }: { className?: string }) {
   return (
     <Link
-      className={clsx("group", className)}
+      className={clsx("group", "fixed bottom-0 end-0")}
       href="https://hotspotty.net"
       target="_blank"
       aria-label="Powered by Hotspotty"
     >
-      <div className="flex flex-row items-center gap-2 rounded-xl bg-white/30 px-4 py-3 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/30 dark:ring-white/10">
-        <HotspottyIcon className="h-6 w-6 text-[#9546ea]" />
-        <span className="whitespace-nowrap font-medium text-zinc-800 group-hover:underline dark:text-zinc-200">
-          Powered by Hotspotty
+      <div className="mb-1 mr-1 flex flex-row items-center gap-2 rounded-xl px-2 py-1 ">
+        <HotspottyIcon className="h-5 w-5 text-[#9546ea]" />
+        <span className="whitespace-nowrap font-medium text-white group-hover:underline dark:text-zinc-200">
+          Hotspotty
         </span>
       </div>
     </Link>
