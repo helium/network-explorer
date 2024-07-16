8f1a0430f7945c9c994961cd9e8c14d1ccfc176b
mbthiery <mbthiery@gmail.com>
2024-07-16 11:18:08 -0400

    Add settings trigger

--- src/app/new/HotspotsMap/SettingsTrigger.tsx
+++ src/app/new/HotspotsMap/SettingsTrigger.tsx
@@ -1,12 +1,60 @@
+import { Dialog, Transition } from "@headlessui/react"
 import { Cog6ToothIcon } from "@heroicons/react/24/outline"
-import Link from "next/link"
+import clsx from "clsx"
+import { Fragment, useCallback, useState } from "react"
+import { Settings } from "../Settings/Settings"
 
 export const SettingsTrigger = () => {
+  const [open, setOpen] = useState(false)
+  const closeSettings = useCallback(() => {
+    setOpen(false)
+  }, [])
+
   return (
     <div className="absolute bottom-6 left-6 flex h-10 w-10 items-center justify-center rounded-xl bg-[#131313]/60">
-      <Link href="/preferences" className="group" aria-label="Preferences">
+      <button onClick={() => setOpen(true)}>
         <Cog6ToothIcon className="h-6 w-6 stroke-[#8B8B8B] transition group-hover:stroke-zinc-700 dark:stroke-zinc-400 group-hover:dark:stroke-zinc-100" />
-      </Link>
+      </button>
+      <Transition.Root
+        show={open}
+        as={Fragment}
+        appear
+        afterLeave={closeSettings}
+      >
+        <Dialog as="div" className="relative z-50" onClose={closeSettings}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 flex items-center justify-center overflow-y-auto p-4 sm:p-6 md:p-24">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel
+                className={clsx(
+                  "transform divide-y divide-gray-500 divide-opacity-10 overflow-hidden bg-opacity-80 shadow-2xl transition-all dark:divide-opacity-20 dark:bg-gray-900 dark:bg-opacity-100"
+                )}
+              >
+                <Settings close={closeSettings} />
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </div>
   )
 }
---
+++ src/app/new/Settings/Settings.tsx
@@ -0,0 +1,46 @@
+import { PreferencesProvider } from "@/context/usePreferences"
+import {
+  ArrowTopRightOnSquareIcon,
+  ChevronRightIcon,
+  Cog6ToothIcon,
+  MoonIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline"
+
+const Divider = () => <div className="w-full border-t border-neutral-400" />
+
+export const Settings = ({ close }: { close: () => void }) => {
+  return (
+    <PreferencesProvider>
+      <div className="flex w-[428px] flex-col gap-3 rounded-xl bg-[#131313]/60 px-8 py-6">
+        <div className="flex justify-between ">
+          <div className="flex items-center gap-2">
+            <Cog6ToothIcon className="h-8 w-8 stroke-neutral-200 dark:stroke-zinc-400" />
+            <p className="text-lg font-medium text-neutral-200">Settings</p>
+          </div>
+          <button onClick={close}>
+            <XMarkIcon className="h-8 w-8 stroke-neutral-200 transition group-hover:stroke-zinc-700 dark:stroke-zinc-400 group-hover:dark:stroke-zinc-100" />
+          </button>
+        </div>
+        <Divider />
+        <button className="flex w-full justify-between">
+          <div className="flex items-center gap-2">
+            <ArrowTopRightOnSquareIcon className="h-6 w-6 stroke-neutral-200 dark:stroke-zinc-400" />
+            <p className="text-base font-medium text-neutral-200">
+              Third-Party Explorer
+            </p>
+          </div>
+          <ChevronRightIcon className="h-6 w-6 stroke-neutral-200 transition group-hover:stroke-zinc-700 dark:stroke-zinc-400 group-hover:dark:stroke-zinc-100" />
+        </button>
+        <Divider />
+        <button className="flex w-full justify-between">
+          <div className="flex items-center gap-2">
+            <MoonIcon className="h-6 w-6 stroke-neutral-200 dark:stroke-zinc-400" />
+            <p className="text-base font-medium text-neutral-200">Mode</p>
+          </div>
+          <ChevronRightIcon className="h-6 w-6 stroke-neutral-200 transition group-hover:stroke-zinc-700 dark:stroke-zinc-400 group-hover:dark:stroke-zinc-100" />
+        </button>
+      </div>
+    </PreferencesProvider>
+  )
+}
