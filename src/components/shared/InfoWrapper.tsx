d275477c1b6d0c50827a5557d92bee601fa710c8
mbthiery <mbthiery@gmail.com>
2024-08-05 12:46:48 -0400

    Tweak scrollbar display

--- src/components/shared/InfoWrapper.tsx
+++ src/components/shared/InfoWrapper.tsx
@@ -3,7 +3,7 @@ import styles from "./InfoWrapper.module.css"
 
 export const InfoWrapper = ({ children }: PropsWithChildren) => (
   <div
-    className={`absolute left-6 top-24 w-80 ${styles.wrapper} overflow-auto rounded-xl`}
+    className={`absolute left-6 top-24 w-80 ${styles.wrapper} overflow-hidden scroll-auto rounded-xl hover:overflow-auto`}
   >
     {children}
   </div>
