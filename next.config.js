260b881efa4ecc7a45406451b0086042fa2aeb70
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2025-01-31 13:42:43 -0800

    Point to world

--- next.config.js
+++ next.config.js
@@ -9,49 +9,14 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/hotspots/hex/:index",
-        destination: "/hex/:index",
-        permanent: false,
+        source: "/((?!api).*)/:slug",
+        destination: "https://world.helium.com",
+        permanent: true,
       },
       {
-        source: "/iot/hex/:index",
-        destination: "/hex/:index",
-        permanent: false,
-      },
-      {
-        source: "/hotspots/:address/activity",
-        destination: "/hotspots/:address",
-        permanent: false,
-      },
-      {
-        source: "/hotspots/:address/witnessed",
-        destination: "/hotspots/:address",
-        permanent: false,
-      },
-      {
-        source: "/hotspots/:address/nearby",
-        destination: "/hotspots/:address",
-        permanent: false,
-      },
-      {
-        source: "/accounts/:address/activity",
-        destination: "/accounts/:address",
-        permanent: false,
-      },
-      {
-        source: "/accounts/:address/hotspots",
-        destination: "/accounts/:address",
-        permanent: false,
-      },
-      {
-        source: "/accounts/:address/validators",
-        destination: "/accounts/:address",
-        permanent: false,
-      },
-      {
-        source: "/market",
-        destination: "/stats",
-        permanent: false,
+        source: "/:slug",
+        destination: "https://world.helium.com",
+        permanent: true,
       },
     ]
   },
